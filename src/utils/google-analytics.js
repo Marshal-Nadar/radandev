7c6f4da02603561fc7999b334de185295c830b7d
Marshal Nadar <marshalnadarmn18@gmail.com>
2024-06-02 18:22:49 +0530

    console removed

--- src/utils/google-analytics.js
+++ src/utils/google-analytics.js
@@ -6,7 +6,7 @@ const initializeGA = () => {
 };
 
 const trackGAEvent = (category, action, label) => {
-  console.log('GA event:', category, ':', action, ':', label);
+  // console.log('GA event:', category, ':', action, ':', label);
   // Send GA4 Event
   ReactGA.event({
     category: category,
